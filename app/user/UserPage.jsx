0572e227ceb76eec16a8027c24647d90669d09b7
Daniel Cary <danieljmcary@gmail.com>
2017-11-04 11:15:53 -0400

    added more panels to user page

--- app/user/UserPage.jsx
+++ app/user/UserPage.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Panel, Form, FormGroup, ControlLabel, FormControl, Col, } from 'react-bootstrap';
+import { Panel, Form, FormGroup, ControlLabel, FormControl, Col, Checkbox, Accordion } from 'react-bootstrap';
 
 
 export default class UserPage extends React.Component {
@@ -12,32 +12,81 @@ export default class UserPage extends React.Component {
     render() {
         return (
             <div>
-                <Panel header="Work Force Pressure Calculator">
-                    <Form horizontal>
-                        <FormGroup>
-                            <Col componentClass={ControlLabel} sm={4}>
-                                Facility
+                <Accordion defaultActiveKey="1">
+                    <Panel header="Work Force Pressure Calculator" eventKey="1">
+                        <Form horizontal>
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={4}>
+                                    Facility
                             </Col>
-                            <Col sm={8}>
-                                <FormControl componentClass="select">
-                                    <option value="1">Facility 1</option>
-                                </FormControl>
+                                <Col sm={8}>
+                                    <FormControl componentClass="select">
+                                        <option value="1">Facility 1</option>
+                                    </FormControl>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={4}>
+                                    Cadres
                             </Col>
-                        </FormGroup>
-                        <FormGroup>
-                            <Col componentClass={ControlLabel} sm={4}>
-                                Cadre
+                                <Col sm={8}>
+                                    <Checkbox checked>Nurse</Checkbox>
+                                    <Checkbox checked>Doctor</Checkbox>
+                                    <Checkbox checked>Whatever</Checkbox>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </Panel>
+                    <Panel header="Predictive Work Force Pressure Calculator" eventKey="2">
+                        <Form horizontal>
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={4}>
+                                    Facility
                             </Col>
-                            <Col sm={8}>
-                                <FormControl componentClass="select">
-                                    <option value="1">Cadre 1</option>
-                                </FormControl>
+                                <Col sm={8}>
+                                    <FormControl componentClass="select">
+                                        <option value="1">Facility 1</option>
+                                    </FormControl>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={4}>
+                                    Cadres
                             </Col>
-                        </FormGroup>
-                    </Form>
-                </Panel>
+                                <Col sm={8}>
+                                    <Checkbox checked>Nurse</Checkbox>
+                                    <Checkbox checked>Doctor</Checkbox>
+                                    <Checkbox checked>Whatever</Checkbox>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </Panel>
+                    <Panel header="Workforce Utilization" eventKey="3">
+                        <Form horizontal>
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={4}>
+                                    Facility
+                            </Col>
+                                <Col sm={8}>
+                                    <FormControl componentClass="select">
+                                        <option value="1">Facility 1</option>
+                                    </FormControl>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={4}>
+                                    Cadres
+                            </Col>
+                                <Col sm={8}>
+                                    <Checkbox checked>Nurse</Checkbox>
+                                    <Checkbox checked>Doctor</Checkbox>
+                                    <Checkbox checked>Whatever</Checkbox>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </Panel>
+                </Accordion>
             </div>
         );
     }
-
 };
