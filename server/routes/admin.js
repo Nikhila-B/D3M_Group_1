bc3700327fc0a84dde497d0808b3371161332602
Daniel Cary <djcary@ncsu.edu>
2017-11-16 11:13:29 -0500

    all the admin api calls are finished except for the update calls and the delete call for tasks. I haven't tested them out yet, might not work

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -26,14 +26,14 @@ router.get('/treatments', (req, res) => {
         .catch(err => res.sendStatus(500));
 });
 
+
+
 // add a new treatment
 router.post('/treatments', (req, res) => {
     new sql.Request()
         .input('Treatment', sql.NVarChar, req.body.name)
         .query('INSERT INTO Treatments (Treatment, Ratio) VALUES (@Treatment, 1); SELECT * FROM Treatments WHERE Id = SCOPE_IDENTITY()')
-        .then(results => {
-            res.json(results.recordset[0]);
-        })
+        .then(results => res.json(results.recordset[0]))
         .catch(err => res.sendStatus(500));
 });
 
@@ -50,44 +50,74 @@ router.delete('/treatments/:id', (req, res) => {
 
 // tasks
 
-// detailed list of tasks for treatment
-router.get('/:treatmentId/tasks', (req, res) => {
+// get all the tasks
+router.get('/tasks', (req, res) => {
     new sql.Request()
-        .input('Id', sql.Int, req.params.treatmentId)
-        .query('SELECT TaskId, Task, CadreId, MinutesPerPatient FROM TreatmentSteps INNER JOIN TimeOnTask ON TreatmentSteps.TaskId = TimeOnTask.Id WHERE TreatmentSteps.TreatmentID=@Id')
+        .query(`SELECT * FROM TimeOnTask`)
         .then(results => {
-            res.json(results.recordset);
-        });
+            // convert to a dictionary instead of array
+            let tasks = {}
+            results.recordset.forEach(row =>
+                tasks[row['Id']] = { task: row['Task'], minutesPerPatient: ['MinutesPerPatient'] }
+            );
+
+            res.json(tasks);
+        })
+        .catch(err => res.sendStatus(500));
 });
 
-// add new task to treatment
-router.post('/:treatmentId/tasks', (req, res) => {
+// add a new task
+router.post('/task', (req, res) => {
     new sql.Request()
-        .input('Task', sql.NVarChar, req.body.taskName)
-        .input('CadreId', sql.Int, req.body.cadreId)
+        .input('Task', sql.NVarChar, req.body.task)
         .input('MinutesPerPatient', sql.Float, req.body.minutesPerPatient)
+        .query(`INSERT INTO TimeOnTask (Task, MinutesPerPatient) VALUES (@Task, @MinutesPerPatient);
+                SELECT * FROM TimeOnTask WHERE Id = SCOPE_IDENTITY()`)
+        .then(results => res.json(results.recordset[0]))
+        .catch(err => res.sendStatus(500));
+});
+
+// steps //
+
+
+// detailed list of steps for treatment
+router.get('/:treatmentId/steps', (req, res) => {
+    new sql.Request()
+        .input('Id', sql.Int, req.params.treatmentId)
+        .query(`SELECT TreatmentSteps.Id, TaskId, Task, CadreId, MinutesPerPatient FROM TreatmentSteps 
+                    INNER JOIN TimeOnTask ON TreatmentSteps.TaskId = TimeOnTask.Id
+                    WHERE TreatmentSteps.TreatmentID=@Id`)
+        .then(results => res.json(results.recordset))
+        .catch(err => res.sendStatus(500));
+});
+
+// add new step to treatment
+router.post('/:treatmentId/steps', (req, res) => {
+    new sql.Request()
         .input('TreatmentId', sql.Int, req.params.treatmentId)
-        .query(`INSERT INTO TimeOnTask (Task, MinutesPerPatient) VALUES (@Task, @MinutesPerPatient); 
-                INSERT INTO TreatmentSteps (TreatmentId, TaskId, CadreId) VALUES (@TreatmentId, SCOPE_IDENTITY(), @CadreId);
-                SELECT TaskId, Task, CadreId, MinutesPerPatient FROM TreatmentSteps 
+        .input('TaskId', sql.NVarChar, req.body.taskId)
+        .input('CadreId', sql.Int, req.body.cadreId)
+        .query(`INSERT INTO TreatmentSteps (TreatmentId, TaskId, CadreId) VALUES (@TreatmentId, @TaskId, @CadreId);
+                SELECT TreatmentSteps.Id, TaskId, Task, CadreId, MinutesPerPatient FROM TreatmentSteps 
                     INNER JOIN TimeOnTask ON TreatmentSteps.TaskId = TimeOnTask.Id
                     WHERE TreatmentSteps.Id = SCOPE_IDENTITY()`)
         .then(results => res.json(results.recordset[0]))
         .catch(err => res.sendStatus(500));
 });
 
-// update task
-router.patch('/:treatmentId/tasks/:taskId', (req, res) => {
+/* update step
+router.patch('/:treatmentId/steps/:stepId', (req, res) => {
     console.log(req.params); // = { treatmentId: 2, taskId: 3 }
 
-});
+});*/
 
-// delete task
-router.delete('/:treatmentId/tasks/:taskId', (req, res) => {
-    console.log(req.params); // = { treatmentId: 2, taskId: 3 }
+// delete step
+router.delete('/:treatmentId/steps/:stepId', (req, res) => {
     new sql.Request()
-        .input('TaskId', sql.Int, req.params.taskId)
-        .query(`DELETE FROM TreatmentSteps WHERE`)
+        .input('Id', sql.Int, req.params.stepId)
+        .query(`DELETE FROM TreatmentSteps WHERE Id = @Id`)
+        .then(() => res.sendStatus(200))
+        .catch(err => res.sendStatus(500));
 });
 
 module.exports = router;
