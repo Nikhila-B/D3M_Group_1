1a8f6e9bb41f6eb8e43760bdbd25023411b303f4
Daniel Cary <djcary@ncsu.edu>
2017-11-27 17:01:22 -0500

    updated lrcharts get requests

--- server/routes/user/predictive.js
+++ server/routes/user/predictive.js
@@ -1,4 +1,5 @@
 const sql = require('mssql');
+const base64url = require('base64-url')
 
 const LinearRegression = require('./predictive-lib/linear_regression').LinearRegression;
 const euclideanDistance = require('./predictive-lib/distance').euclidean;
@@ -48,22 +49,49 @@ router.post('/', (req, res) => {
     });
 });
 
+// get list of indicators
+router.get('/lrcharts/indicators', (req, res) => {
+    new sql.Request()
+        .query(`SELECT k.tagword, t.[Indicator Name] as indicator
+                FROM [dbo].[keyWords] k, (select * from [dbo].[WDIData] where [Country Code] = 'UGA') as t
+                WHERE t.[Indicator Name] LIKE '%'+ k.tagword +'%';`)
+        .then(result => {
+            // group all the indicators by tagword
+            let tagwords = {};
+            result.recordset.forEach(row => {
+                if(tagwords[row['tagword']] == null) {
+                    tagwords[row['tagword']] = [];
+                }
+                tagwords[row['tagword']].push(row['indicator']);
+            });
+
+            res.json(tagwords);
+        }).catch(err => res.status(500).json(err));
+});
+
+
 /*get all the rows/relevant records related to healthcare categories for LR chart
 ------ x-axis = will be years [2000 - 2016] | predict[upto 2020] : FIXED
 ------ y-axis = will be the numbers values : need to edit */
-router.get('/LRcharts', (req, res) => {
-    new sql.Request()
-        .query(`select k.tagword, t.[Indicator Name], t.[Country Name], t.[2000],
-        t.[2001], t.[2002], t.[2003], t.[2004], t.[2005],
-        t.[2006], t.[2007], t.[2008],t.[2009], t.[2010],
-        t.[2011], t.[2012], t.[2013], t.[2014], t.[2015],
-        t.[2016]
-        from [dbo].[keyWords] k, (select * from [dbo].[WDIData] where [Country Code] = 'UGA') as t
-        where t.[Indicator Name] LIKE '%'+ k.tagword +'%';'`)
-        .then(result => res.json(result.recordset))
-        .catch(err => res.sendStatus(500));
-});
+router.get('/lrcharts/:indicator', (req, res) => {
+    let indicator = base64url.decode(req.params.indicator);
 
+    new sql.Request()
+        .input('Indicator', sql.NVarChar, indicator)
+        .query(`SELECT [2000], [2001], [2002], [2003], [2004],
+                    [2005], [2006], [2007], [2008],[2009], [2010],
+                    [2011], [2012], [2013], [2014], [2015], [2016] FROM WDIData 
+                WHERE [Country Code] = 'UGA' 
+                    AND [Indicator Name] = @Indicator`)
+        .then(result => {
+
+            let data = result.recordset[0];
+
+            // perform linear regression
+
+            res.json(data);
+        }).catch(err => console.log(err))
+});
 
 
 module.exports = router;
